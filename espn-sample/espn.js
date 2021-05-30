bb0eae6ebfa5af161a77b888e2f2940329834bcd
selea <sve5040@gmail.com>
2021-05-30 16:52:47 -0500

    added loop to update headlines

---
+++ espn-sample/espn.js
@@ -0,0 +1,44 @@
+
+
+document.getElementById('submBtnOne').addEventListener('click', function(event){
+    event.preventDefault()
+    var randomNumb = Math.floor(Math.random() * 2)
+    console.log("random numb = " + randomNumb)
+    var userInput = document.getElementById('fingersNumb').value
+    console.log(userInput)
+
+
+    //check to see if numbers are equal
+
+    if(userInput == randomNumb){
+        console.log('You won')
+    }else{
+        console.log('You lost')
+    }
+
+
+    //LOOP OVER FIVE
+    for(var i = 0; i < 5; i++){
+        console.log(i)
+    }
+
+    for(var i = 10; i > 2; i--){
+        console.log(i)
+    }
+
+})
+
+var headlines = ["headlines one", "headlines two"]
+
+var headlinesString = ""
+
+document.getElementById('updateBtn').addEventListener('click', function(event){
+    event.preventDefault()
+    for(i=0; i<headlines.length; i++){
+        console.log(headlines[i])
+
+        headlinesString = headlinesString + "<p>" + headlines[i] +"</p>"
+        // console.log(headlinesString)
+    }
+    document.getElementById('updatedLines').innerHTML = headlinesString 
+})
